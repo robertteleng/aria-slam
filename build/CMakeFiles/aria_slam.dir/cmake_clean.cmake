ae66eb7ef252b49f4dad1fcb10ac57c5ef174de3
robertteleng <rrojo.va@gmail.com>
2025-12-19 17:48:54 +0100

    feat: initial setup with CMake and Hello World

---
+++ build/CMakeFiles/aria_slam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aria_slam.dir/main.cpp.o"
+  "CMakeFiles/aria_slam.dir/main.cpp.o.d"
+  "aria_slam"
+  "aria_slam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aria_slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
