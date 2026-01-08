02149f9ef8d6a02699ce8fd58a5269d6daf5ce9e
robertteleng <rrojo.va@gmail.com>
2026-01-08 15:47:13 +0100

    feat(frame): add ORB feature members

--- include/Frame.hpp
+++ include/Frame.hpp
@@ -1,9 +1,17 @@
 #pragma once
 #include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
+#include <vector>
 
 class Frame {
 public:
     Frame(const cv::Mat& image);
+    void extractFeatures();
     
     cv::Mat image_;
+    std::vector<cv::KeyPoint> keypoints_;
+    cv::Mat descriptors_;
+
+private:
+    cv::Ptr<cv::ORB> orb_;
 };
