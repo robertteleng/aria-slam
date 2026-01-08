c5af4415825f8b1d5f09de18e26b299cb6a0f0dc
robertteleng <rrojo.va@gmail.com>
2026-01-08 16:00:55 +0100

    feat(map): add PLY export method

--- include/Mapper.hpp
+++ include/Mapper.hpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <Eigen/Dense>
+#include <string>
 
 struct MapPoint {
     Eigen::Vector3d position;
@@ -14,7 +15,10 @@ public:
     
     void triangulatePoints(const std::vector<cv::Point2f>& pts1,
                            const std::vector<cv::Point2f>& pts2,
-                           const cv::Mat& P1, const cv::Mat& P2);
+                           const cv::Mat& P1, const cv::Mat& P2,
+                           const cv::Mat& img1);
+    
+    void savePLY(const std::string& filename) const;
     
     std::vector<MapPoint> points_;
 };
