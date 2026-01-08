598bf738278909db2b92a6360c211a1eaad3505c
robertteleng <rrojo.va@gmail.com>
2026-01-08 15:47:27 +0100

    feat(frame): implement CPU ORB extraction

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,4 +1,16 @@
 #include "Frame.hpp"
 
 Frame::Frame(const cv::Mat& image) : image_(image.clone()) {
+    orb_ = cv::ORB::create(2000);
+}
+
+void Frame::extractFeatures() {
+    cv::Mat gray;
+    if (image_.channels() == 3) {
+        cv::cvtColor(image_, gray, cv::COLOR_BGR2GRAY);
+    } else {
+        gray = image_;
+    }
+    
+    orb_->detectAndCompute(gray, cv::noArray(), keypoints_, descriptors_);
 }
