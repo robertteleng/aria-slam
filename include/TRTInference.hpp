258ab8c53fe33e4a6060bbe799fd40465b108577
robertteleng <rrojo.va@gmail.com>
2026-01-08 15:55:36 +0100

    feat(yolo): add async detection with CUDA stream

--- include/TRTInference.hpp
+++ include/TRTInference.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/cuda.hpp>
 #include <NvInfer.h>
 #include <cuda_runtime.h>
 
@@ -17,6 +18,8 @@ public:
     ~TRTInference();
     
     std::vector<Detection> detect(const cv::Mat& image);
+    void detectAsync(const cv::Mat& image, cudaStream_t stream);
+    std::vector<Detection> getResults();
     
 private:
     nvinfer1::IRuntime* runtime_;
@@ -25,5 +28,12 @@ private:
     
     void* buffers_[2];
     int inputIndex_, outputIndex_;
-    int inputSize_, outputSize_;
+    size_t inputSize_, outputSize_;
+    
+    std::vector<float> outputData_;
+    int inputH_, inputW_;
+    float scaleX_, scaleY_;
+    
+    void preprocess(const cv::Mat& image, float* buffer);
+    std::vector<Detection> postprocess();
 };
