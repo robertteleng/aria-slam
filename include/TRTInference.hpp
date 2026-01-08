a4dd4c8fb3783f02e831c52b8056968a15462df2
robertteleng <rrojo.va@gmail.com>
2026-01-08 15:55:07 +0100

    feat(yolo): add TensorRT engine members

--- include/TRTInference.hpp
+++ include/TRTInference.hpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <NvInfer.h>
+#include <cuda_runtime.h>
 
 struct Detection {
     int classId;
@@ -15,4 +17,13 @@ public:
     ~TRTInference();
     
     std::vector<Detection> detect(const cv::Mat& image);
+    
+private:
+    nvinfer1::IRuntime* runtime_;
+    nvinfer1::ICudaEngine* engine_;
+    nvinfer1::IExecutionContext* context_;
+    
+    void* buffers_[2];
+    int inputIndex_, outputIndex_;
+    int inputSize_, outputSize_;
 };
